976c69021dfa575abff47dad6c99c381784f8d17
Carlos Alberto <caralbertverso@gmail.com>
2023-10-14 04:12:29 -0300

    Config socket.io

--- routes.ts
+++ routes.ts
@@ -2,11 +2,17 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./src/controllers/AuthenticateUserController";
 import { CreateMessageController } from "./src/controllers/CreateMessageController";
 import { ensureAuthenticated } from "./src/middleware/ensureAuthenticated";
+import { GetLast3MessagesController } from "./src/controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./src/controllers/ProfileUserController";
 
 const router = Router()
 
 router.post("/authenticate", new AuthenticateUserController().handle)
 
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
+
+router.get("/messages/last3", new GetLast3MessagesController().handle)
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle)
 
 export { router }
